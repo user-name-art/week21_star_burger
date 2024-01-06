70e775670e7e6cad9676c1c5720564d626b554d7
Art Gilyazov <art.gilyazov@mail.ru>
2024-01-06 07:00:45 +0300

    extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Product, Order, OrderedProduct
+import phonenumbers
 
 
 def banners_list_api(request):
@@ -62,23 +63,41 @@ def product_list_api(request):
 def register_order(request):
     received_order = request.data
 
-    if 'products' in received_order and isinstance(received_order['products'], list) and received_order['products']:
-        order = Order.objects.create(
-            first_name=received_order['firstname'],
-            last_name=received_order['lastname'],
-            phonenumber=received_order['phonenumber'],
-            address=received_order['address']
-        )
+    products = Product.objects.available()
+    product_ids = [product.id for product in products]
 
-        products = Product.objects.available()
+    if 'firstname' in received_order and 'lastname' in received_order and 'phonenumber' in received_order and 'address' in received_order:
+        if isinstance(received_order['firstname'], str):
+            if received_order['firstname'] and received_order['lastname'] and received_order['phonenumber'] and received_order['address']:
+                pure_phonenumber = phonenumbers.parse(received_order['phonenumber'], None)
+                if not phonenumbers.is_valid_number(pure_phonenumber):
+                    return Response({'error': 'invalid phonenumber'}, status=status.HTTP_400_BAD_REQUEST)
+                if 'products' in received_order and isinstance(received_order['products'], list) and received_order['products']:
+                    for product in received_order['products']:
+                        if product['product'] in product_ids:
+                            ordered_products = {product['product']: product['quantity']}
+                        else:
+                            return Response({'error': 'invalid product id'}, status=status.HTTP_400_BAD_REQUEST)
+                    order = Order.objects.create(
+                        first_name=received_order['firstname'],
+                        last_name=received_order['lastname'],
+                        phonenumber=received_order['phonenumber'],
+                        address=received_order['address']
+                    )
 
-        for product in received_order['products']:
-            OrderedProduct.objects.create(
-                product=products.get(id=product['product']),
-                quantity=product['quantity'],
-                order=order
-            )
+                    for product in received_order['products']:
+                        OrderedProduct.objects.create(
+                            product=products.get(id=product['product']),
+                            quantity=product['quantity'],
+                            order=order
+                        )
+                else:
+                    return Response({'error': 'products must be non-empty list'}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'error': 'firstname, lastname, phonenumber, address: поле не может быть пустым'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'firstname must be str'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response({'error': 'products must be non-empty list'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'firstname, lastname, phonenumber, address: Обязательное поле'}, status=status.HTTP_400_BAD_REQUEST)
 
-    return Response({})
+    return Response({}, status=status.HTTP_201_CREATED)
