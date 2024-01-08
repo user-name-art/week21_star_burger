7554f64042a2c3ab31d703dc1063c7396530af00
Art Gilyazov <art.gilyazov@mail.ru>
2024-01-08 16:08:40 +0300

    add redırect from admın to manager ınterface

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
+from django.utils.http import url_has_allowed_host_and_scheme
+from star_burger.settings import ALLOWED_HOSTS
 
 from .models import Product
 from .models import ProductCategory
@@ -116,3 +119,11 @@ class OrderedProductsInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderedProductsInline]
     list_display = ['firstname', 'lastname', 'phonenumber']
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+
+        if 'next' in request.GET and url_has_allowed_host_and_scheme(request.GET['next'], ALLOWED_HOSTS):
+            return HttpResponseRedirect(request.GET['next'])
+
+        return res
