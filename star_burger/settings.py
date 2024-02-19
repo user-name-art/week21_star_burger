5d298911d5d83a4743753c7dce3bddd30f3c1e2e
Art Gilyazov <art.gilyazov@mail.ru>
2024-02-20 00:01:16 +0300

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -85,9 +85,11 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+DB_URL = env('DB_URL')
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        default=DB_URL
     )
 }
 
